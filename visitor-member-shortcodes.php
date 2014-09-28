256824118e749df85eff5ac25f2afa7cc48435bc
Eric King <eric@webdeveric.com>
2014-09-28 00:13:03 -0500

    CS updates

--- visitor-member-shortcodes.php
+++ visitor-member-shortcodes.php
@@ -1,32 +1,36 @@
 <?php
 /*
 Plugin Name: Visitor/Member Shortcodes
+Plugin Group: Shortcodes
 Version: 0.1.1
 Description: This plugin provides shortcodes to show content if the user is logged in or not.
 Author: Eric King
 Author URI: http://webdeveric.com
 */
 
-class VisitorMemberShortcodes {
-
-	public static function init() {
+class VisitorMemberShortcodes
+{
+	public static function init()
+    {
 		add_shortcode('visitor', array( __CLASS__, 'visitor') );
 		add_shortcode('member', array( __CLASS__, 'member') );
 	}
 
-	private static function shortcode( $atts, $content = null, $code = null, $condition = false ){
+	private static function shortcode( $atts, $content = null, $shortcode_name = null, $condition = false )
+    {
 		if( ! isset( $content ) )
 			return '';
 		return $condition ? do_shortcode( $content ) : '';
 	}
 
-	public static function visitor( $atts, $content = null, $code = null ){
-		return self::shortcode( $atts, $content, $code, ! is_user_logged_in() );
+	public static function visitor( $atts, $content = null, $shortcode_name = null )
+    {
+		return self::shortcode( $atts, $content, $shortcode_name, ! is_user_logged_in() );
 	}
 
-	public static function member( $atts, $content = null, $code =  null ){
-		return self::shortcode( $atts, $content, $code, is_user_logged_in() );
+	public static function member( $atts, $content = null, $shortcode_name =  null )
+    {
+		return self::shortcode( $atts, $content, $shortcode_name, is_user_logged_in() );
 	}
-
 }
 VisitorMemberShortcodes::init();
